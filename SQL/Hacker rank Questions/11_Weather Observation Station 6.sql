6eb8928069170c7d3ae3d17ae485e437b93ceff8
Darky001 <98139553+Atharv-Nalwade@users.noreply.github.com>
2022-09-02 10:48:04 +0530

    Create 11_Weather Observation Station 6.sql

---
+++ SQL/Hacker rank Questions/11_Weather Observation Station 6.sql
@@ -0,0 +1,3 @@
+ select distinct CITY from STATION where (city like "a%" or city like "e%" or city like "i%" or city like "o%" or city like "u%");
+ 
+ #https://www.hackerrank.com/challenges/weather-observation-station-6/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
