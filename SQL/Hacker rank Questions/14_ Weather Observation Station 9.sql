e54cd76fcb2fd2b840557efcf8269d06d64b56b6
Darky001 <98139553+Atharv-Nalwade@users.noreply.github.com>
2022-09-03 00:48:50 +0530

    Create 14_ Weather Observation Station 9.sql

---
+++ SQL/Hacker rank Questions/14_ Weather Observation Station 9.sql
@@ -0,0 +1,3 @@
+select distinct city from STATION where  SUBSTR(city,1,1) not in ('a','e','i','o','u');
+
+#https://www.hackerrank.com/challenges/weather-observation-station-9/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
