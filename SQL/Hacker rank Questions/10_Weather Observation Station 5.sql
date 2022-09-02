4c3163270720f015accd91bcfc581527deec210a
Darky001 <98139553+Atharv-Nalwade@users.noreply.github.com>
2022-09-02 10:39:22 +0530

    Create 10_Weather Observation Station 5.sql

---
+++ SQL/Hacker rank Questions/10_Weather Observation Station 5.sql
@@ -0,0 +1,4 @@
+select city,CHAR_LENGTH(city)  from station order by CHAR_LENGTH(city),city ASC limit 1;
+select city,CHAR_LENGTH(city) from station order by CHAR_LENGTH(city) DESC limit 1;
+
+#https://www.hackerrank.com/challenges/weather-observation-station-5/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen;
