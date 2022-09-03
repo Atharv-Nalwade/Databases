d29d8884d4d6783bd18db7efc3640ff680fae971
Darky001 <98139553+Atharv-Nalwade@users.noreply.github.com>
2022-09-03 19:16:38 +0530

    Create 26_Average Population.sql
    Use ROUND() function

---
+++ SQL/Hacker rank Questions/26_Average Population.sql
@@ -0,0 +1,9 @@
+# CAST(expression AS datatype(length))
+
+#The SQL ROUND() function is used to round a numeric field (floating point value) to the whole number (integer value without fractional) of decimals specified.
+
+#The SQL ROUND() function is converting a floating point value to nearest integer value.
+
+select ROUND(AVG(POPULATION) ) from CITY ;
+
+#https://www.hackerrank.com/challenges/average-population/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
