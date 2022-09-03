4d7dc8d0a2ed6b4049f586ca05e33e51d5dbf157
Darky001 <98139553+Atharv-Nalwade@users.noreply.github.com>
2022-09-03 19:08:14 +0530

    Create 24_Revisiting Aggregations-The SUM Function.sql

---
+++ SQL/Hacker rank Questions/24_Revisiting Aggregations-The SUM Function.sql
@@ -0,0 +1,3 @@
+select SUM(population) from CITY where DISTRICT="California";
+
+#https://www.hackerrank.com/challenges/revising-aggregations-sum/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
