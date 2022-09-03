8a7d83969196cf923fc39b0e00cf46ebae3733dc
Darky001 <98139553+Atharv-Nalwade@users.noreply.github.com>
2022-09-03 11:39:07 +0530

    Create 20_Employee Salaries.sql

---
+++ SQL/Hacker rank Questions/20_Employee Salaries.sql
@@ -0,0 +1,6 @@
+/*
+Enter your query here.
+*/
+select name from Employee where salary>2000 and months<10 order by employee_id;
+
+#https://www.hackerrank.com/challenges/salary-of-employees/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
