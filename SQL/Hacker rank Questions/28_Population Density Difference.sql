c0179be3d76bde91b3f7acb2d9b2b64a678d27c3
Darky001 <98139553+Atharv-Nalwade@users.noreply.github.com>
2022-09-03 19:19:50 +0530

    Create 28_Population Density Difference.sql

---
+++ SQL/Hacker rank Questions/28_Population Density Difference.sql
@@ -0,0 +1,3 @@
+select max(population)-min(population) from CITY;
+
+#https://www.hackerrank.com/challenges/population-density-difference/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
