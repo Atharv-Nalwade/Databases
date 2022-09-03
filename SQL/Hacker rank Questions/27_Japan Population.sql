18a131fada554060841bf208ff5a301a7c4227af
Darky001 <98139553+Atharv-Nalwade@users.noreply.github.com>
2022-09-03 19:18:21 +0530

    Create 27_Japan Population.sql

---
+++ SQL/Hacker rank Questions/27_Japan Population.sql
@@ -0,0 +1,3 @@
+select sum(population) from CITY where COUNTRYCODE="JPN";
+
+#https://www.hackerrank.com/challenges/japan-population/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
