e7891f1a82277a3ef65b1cef2a97e6ec473d6f91
Darky001 <98139553+Atharv-Nalwade@users.noreply.github.com>
2022-09-03 19:41:06 +0530

    Create 29_The Blunder.sql
    Use REPLACE() Function works for INTEGER Also

---
+++ SQL/29_The Blunder.sql
@@ -0,0 +1,5 @@
+select  ROUND(avg(salary)) -
+                  ROUND(AVG(REPLACE(Salary,'0','')))
+from EMPLOYEES;
+
+#https://www.hackerrank.com/challenges/the-blunder/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
