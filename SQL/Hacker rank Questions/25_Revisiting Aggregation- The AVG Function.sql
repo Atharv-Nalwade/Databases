ba8746e1fba78d236a910e8d12ad47472c4c98e8
Darky001 <98139553+Atharv-Nalwade@users.noreply.github.com>
2022-09-03 19:09:47 +0530

    Create 25_Revisiting Aggregation- The AVG Function.sql

---
+++ SQL/Hacker rank Questions/25_Revisiting Aggregation- The AVG Function.sql
@@ -0,0 +1,3 @@
+select avg(population) from CITY where DISTRICT="California";
+
+#https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
