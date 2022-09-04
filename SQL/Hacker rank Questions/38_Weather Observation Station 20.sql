cc90ab525d72d2ecc5945e1d8084ec27c5fee92a
Darky001 <98139553+Atharv-Nalwade@users.noreply.github.com>
2022-09-04 16:22:23 +0530

    *** REVISIT ***

---
+++ SQL/Hacker rank Questions/38_Weather Observation Station 20.sql
@@ -0,0 +1,5 @@
+select round(lat_n,4) from STATION Order by LAT_N  limit 1 offset 249;
+
+#SELECT ROUND(LAT_N,4) FROM STATION ORDER BY LAT_N LIMIT 1 OFFSET 249
+
+#https://www.hackerrank.com/challenges/weather-observation-station-20/problem
