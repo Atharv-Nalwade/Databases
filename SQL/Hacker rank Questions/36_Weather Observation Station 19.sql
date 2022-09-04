67d860d2551900ad1be6ce4038bcf64db7078bc1
Darky001 <98139553+Atharv-Nalwade@users.noreply.github.com>
2022-09-04 12:15:19 +0530

    Create 36_Weather Observation Station 19.sql
    ABS() function for the absolute values

---
+++ SQL/Hacker rank Questions/36_Weather Observation Station 19.sql
@@ -0,0 +1,4 @@
+SELECT ROUND(ABS(MAX(lat_n)-MIN(lat_n))+ABS(MAX(long_w)-MIN(long_w)),4)
+FROM station
+
+#https://www.hackerrank.com/challenges/weather-observation-station-18/problem?h_r=next-challenge&h_r=next-challenge&h_v=zen&h_v=zen&isFullScreen=true&h_r=next-challenge&h_v=zen
