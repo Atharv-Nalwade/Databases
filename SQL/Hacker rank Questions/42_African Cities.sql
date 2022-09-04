932d922e999d69579180ec0083992b661168435e
Darky001 <98139553+Atharv-Nalwade@users.noreply.github.com>
2022-09-04 22:27:27 +0530

    Create 42_African Cities.sql

---
+++ SQL/Hacker rank Questions/42_African Cities.sql
@@ -0,0 +1,3 @@
+select CITY.NAME from CITY join COUNTRY on CITY.CountryCode=COUNTRY.Code where continent="Africa";
+
+#https://www.hackerrank.com/challenges/african-cities/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
