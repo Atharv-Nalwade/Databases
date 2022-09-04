0e92fbefe27c72634e7fef9b41988b447b89aa31
Darky001 <98139553+Atharv-Nalwade@users.noreply.github.com>
2022-09-04 09:24:24 +0530

    Create 32_Weather Observation Station 14.sql
    Uses Truncate function imp

---
+++ SQL/Hacker rank Questions/32_Weather Observation Station 14.sql
@@ -0,0 +1,3 @@
+select TRUNCATE(max(lat_n),4) from STATION where lat_n<137.2345;
+
+#https://www.hackerrank.com/challenges/weather-observation-station-14/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
