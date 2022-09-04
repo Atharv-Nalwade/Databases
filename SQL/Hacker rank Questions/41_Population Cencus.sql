6174c7443119ebb0c396dd4ee9db7fbdc6057555
Darky001 <98139553+Atharv-Nalwade@users.noreply.github.com>
2022-09-04 22:24:46 +0530

    Create 41_Population Cencus.sql

---
+++ SQL/Hacker rank Questions/41_Population Cencus.sql
@@ -0,0 +1,3 @@
+select sum(CITY.population) from CITY join COUNTRY on CITY.CountryCode=COUNTRY.Code where CONTINENT="Asia"
+
+#https://www.hackerrank.com/challenges/asian-population/problem?isFullScreen=true
